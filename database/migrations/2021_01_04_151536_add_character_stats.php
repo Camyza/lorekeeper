8077aeb9fa19e0edda34dd2b7a1e62ddaef999f1
camyza <rayesesshyfan@gmail.com>
2025-01-01 02:42:43 -0600

    feat(stats): add fontawesome icon nullable field to stats

--- database/migrations/2021_01_04_151536_add_character_stats.php
+++ database/migrations/2021_01_04_151536_add_character_stats.php
@@ -14,6 +14,7 @@ class AddCharacterStats extends Migration {
             $table->increments('id');
             $table->string('name');
             $table->string('abbreviation');
+            $table->string('icon', 100)->nullable();
             $table->integer('default')->unsigned();
             // step increase so it doesnt have to increase by one
             $table->integer('step')->unsigned()->nullable();
