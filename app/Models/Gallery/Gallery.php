02721a0f65a7f1162a5a9c30cd23712193038969
Camyza <Camyza@users.noreply.github.com>
2026-09-08 19:23:51 +0000

    refactor: fix PHP styling

--- app/Models/Gallery/Gallery.php
+++ app/Models/Gallery/Gallery.php
@@ -102,9 +102,9 @@ class Gallery extends Model {
     public function submissions() {
         return $this->hasMany(GallerySubmission::class, 'gallery_id')->visible()->orderBy('created_at', 'DESC');
     }
-	
-	/**
-	* Get the submissions that belong specifically
+
+    /**
+     * Get the submissions that belong specifically
      * to children of this gallery.
      * (This is a builder, not a relation. Just put
      * up here to be next to the submissions relation.).
